c00882285109d746e3462e63a2c667443621fc93
Rafael Lino <126628341+rafak7@users.noreply.github.com>
2024-04-29 11:27:56 -0300

    Update TipoVariavies.java

--- tipos-variavies/src/TipoVariavies.java
+++ tipos-variavies/src/TipoVariavies.java
@@ -1,5 +1,21 @@
 public class TipoVariavies {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+         final double VALOR_DE_PI = 3.14;
+
+        //VALOR_DE_PI = 10.75;
+
+        System.out.println(VALOR_DE_PI);
     }
 }
