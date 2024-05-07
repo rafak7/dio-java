e467b7ee91f7a3a0e34a55e1b3c335b21c2ce0c5
Rafael Lino <126628341+rafak7@users.noreply.github.com>
2024-05-07 14:10:19 -0300

    Add files via upload

---
+++ Controle-Fluxo/src/main/java/org/example/ExemploForArray.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos [] = {"RAFAEL", "MARIA", "RENATO"};
+
+        for (int x=0; x<alunos.length; x++){
+            System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+        }
+
+
+    }
+}
